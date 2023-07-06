cad87f3dcfba481b6a3c40b335fb412580622df7
ArjunreddyR <138602403+ArjunreddyR@users.noreply.github.com>
2023-07-06 20:54:03 +0530

    Date_7_6_2023/

---
+++ Date_7_6_2023/main.py
@@ -0,0 +1,31 @@
+#variable
+a=10
+print(a)
+#list
+b=['a','b','c','d']
+print(a)
+#tuple
+c=(1,'x',3,'t',5)
+print(a)
+#dictionary
+d={
+    'name':'arjun'
+}
+
+
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
